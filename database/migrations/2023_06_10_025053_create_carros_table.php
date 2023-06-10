66c0aaa55c71c38e102452aec1e0641cb8112c75
Elivelton <elivelton.gbi@gmail.com>
2023-06-09 23:56:48 -0300

    up 3

--- database/migrations/2023_06_10_025053_create_carros_table.php
+++ database/migrations/2023_06_10_025053_create_carros_table.php
@@ -13,6 +13,7 @@ return new class extends Migration
     {
         Schema::create('carros', function (Blueprint $table) {
             $table->id();
+            $table->string();
             $table->timestamps();
         });
     }
